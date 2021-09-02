16bc8626539894bbf25219bdcb414ea81d16adf4
AnastasiaPerkal <nastya.perkal@gmail.com>
2021-09-02 10:11:35 +0300

    Added spring project

---
+++ SpringFirstApp/src/test/java/by/perkal/SpringFirstApp/SpringFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.perkal.SpringFirstApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
