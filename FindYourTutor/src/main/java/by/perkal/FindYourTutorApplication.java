7f56e149512c0dd090a7d9ec0c7bc179c4715862
AnastasiaPerkal <nastya.perkal@gmail.com>
2021-10-27 09:48:24 +0300

    Project created

---
+++ FindYourTutor/src/main/java/by/perkal/FindYourTutorApplication.java
@@ -0,0 +1,13 @@
+package by.perkal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindYourTutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindYourTutorApplication.class, args);
+    }
+
+}
---
+++ FindYourTutor/src/test/java/by/perkal/FindYourTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package by.perkal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindYourTutorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
